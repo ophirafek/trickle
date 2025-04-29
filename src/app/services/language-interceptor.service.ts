c4dccec11f321038dfbdb0de641e34d8814db271
ophirafekm <ophir.afek@gmail.com>
2025-04-29 13:27:19 +0300

    complete translation

--- src/app/services/language-interceptor.service.ts
+++ src/app/services/language-interceptor.service.ts
@@ -5,15 +5,17 @@ import { TranslocoService } from '@ngneat/transloco';
 
 @Injectable()
 export class LanguageInterceptor implements HttpInterceptor {
+
+  langs : Record<string, string> = {'en': 'en-US', 'he': 'he-IL'}; // Example language mapping
   constructor(private translocoService: TranslocoService) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     // Get current language from Transloco
     const currentLang = this.translocoService.getActiveLang();
-    
+
     // Clone the request and add the language header
     const modifiedReq = req.clone({
-      headers: req.headers.set('Accept-Language', currentLang)
+      headers: req.headers.set('Accept-Language', this.langs[currentLang])
     });
     
     // Pass the modified request to the next handler
